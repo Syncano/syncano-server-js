428ad994563304043ffee751963e3b3a9421fbbf
Kasper Mikiewicz <ideredpl@gmail.com>
2016-12-21 16:46:24 +0100

    feat: add #firstOrFail tests

--- test/lib/dataTest.js
+++ test/lib/dataTest.js
@@ -3,7 +3,7 @@ import should from 'should/as-function';
 import Syncano from 'syncano'
 import SyncanoORM from '../../src';
 
-// import { NotFoundError } from '../../src/errors';
+import { NotFoundError } from '../../src/errors';
 
 describe('Data', function() {
   const testUrl = 'https://api.syncano.rocks';
@@ -57,12 +57,19 @@ describe('Data', function() {
         });
 
       data.users.take(10).list().then(objects => {
-        should(objects).deepEqual([{
-          className: 'users',
-          id: 3,
-          instanceName: 'testInstance',
-          name: 'John Doe'
-        }])
+        should(objects).be.Array().length(1)
+        should(objects).have.propertyByPath(0, 'name').which.is.String();
+        should(objects).have.propertyByPath(0, 'id').which.is.Number();
+      })
+    });
+
+    it('should return [] when no objects were not found', function() {
+      api.get(`/v1.1/instances/${instanceName}/classes/users/objects/`, '*')
+        .query({ page_size: 5 })
+        .reply(200, { objects: [], next: null });
+
+      data.users.take(5).list().then(objects => {
+        should(objects).be.Array().empty()
       })
     });
   });
@@ -78,8 +85,95 @@ describe('Data', function() {
         .reply(200, { objects: [{ name: 'John Doe', id: 3 }] });
 
       data.users.first().then(object => {
-        should(object).deepEqual({ id: 3, name: 'John Doe' })
+        should(object).be.Object()
+        should(object).have.property('name').which.is.String();
+        should(object).have.property('id').which.is.Number();
       })
+    });
+
+    it('should return null when object was not found', function() {
+      api.get(`/v1.1/instances/${instanceName}/classes/users/objects/`, '*')
+        .query({ page_size: 1 })
+        .reply(200, { objects: [] });
+
+      data.users.first().then(object => should(object).be.Null())
+    });
+  });
+
+  describe('#firstOrFail()', function() {
+    it('should be a method of the model', function() {
+      should(data.users).have.property('firstOrFail').which.is.Function();
+    });
+
+    it('should be able to fetch single object', function() {
+      api.get(`/v1.1/instances/${instanceName}/classes/users/objects/`, '*')
+        .query({ page_size: 1 })
+        .reply(200, { objects: [{ name: 'John Doe', id: 3 }] });
+
+      data.users.first().then(object => {
+        should(object).be.Object()
+        should(object).have.property('name').which.is.String();
+        should(object).have.property('id').which.is.Number();
+      })
+    });
+
+    it('should throw error when object was not found', function() {
+      api.get(`/v1.1/instances/${instanceName}/classes/users/objects/`, '*')
+        .query({ page_size: 1 })
+        .reply(404);
+
+      should(data.users.first()).rejectedWith(NotFoundError)
+    });
+  });
+
+  describe('#find()', function() {
+    it('should be a method of the model', function() {
+      should(data.users).have.property('find').which.is.Function();
+    });
+
+    it('should be able to fetch single object', function() {
+      api.get(`/v1.1/instances/${instanceName}/classes/users/objects/`, '*')
+        .query({
+          query: JSON.stringify({ id: { _eq: 7 }}),
+          page_size: 1
+        })
+        .reply(200, { objects: [{ name: 'John Doe', id: 7 }] });
+
+      data.users.find(7).then(object => {
+        should(object).be.Object()
+        should(object).have.property('name').which.is.String();
+        should(object).have.property('id').which.is.Number();
+      })
+    });
+
+    it('should be able to fetch objects list', function() {
+      api.get(`/v1.1/instances/${instanceName}/classes/users/objects/`, '*')
+        .query({
+          query: JSON.stringify({ id: { _in: [7, 8] }})
+        })
+        .reply(200, { objects: [
+          { name: 'John Doe', id: 7 },
+          { name: 'Jane Doe', id: 8 }
+        ], next: null });
+
+      data.users.find([7, 8]).then(objects => {
+        should(objects).be.Array().length(2)
+        should(objects).have.propertyByPath(0, 'name').which.is.String();
+        should(objects).have.propertyByPath(0, 'id').which.is.Number();
+        should(objects).have.propertyByPath(1, 'name').which.is.String();
+        should(objects).have.propertyByPath(1, 'id').which.is.Number();
+      })
+    });
+
+    it('should return null when object was not found', function() {
+      api.get(`/v1.1/instances/${instanceName}/classes/users/objects/`, '*')
+        .query({
+          query: JSON.stringify({ id: { _eq: 5 }}),
+          page_size: 1
+        })
+        .reply(200, { objects: [], next: null });
+
+      data.users.find(5).then(object => should(object).be.Null())
     });
   });
 });
