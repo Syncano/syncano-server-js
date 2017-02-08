6be139bbbc68f7e150c507635924af3bdb19d4fe
Maciej Kucharz <m@kucharz.net>
2017-02-08 02:32:57 +0100

    Fix/overriding env (#13)
    * fix(envs): ovveriding env in proper way
    
    * fix(xo): fixing style

--- src/settings.js
+++ src/settings.js
@@ -1,2 +1,4 @@
-export const SYNCANO_HOST = process.env.SYNCANO_HOST || 'api.syncano.io'
+export const getHost = function () {
+  return process.env.SYNCANO_HOST || 'api.syncano.io'
+}
 export const SYNCANO_API_VERSION = 'v2'
