1771b7319ad8030435b0c787c1178d36893b9bab
Kasper Mikiewicz <ideredpl@gmail.com>
2017-09-15 17:33:55 +0200

    fix: handle global META

--- src/settings.js
+++ src/settings.js
@@ -1,8 +1,26 @@
-export default ({meta = {}, token, instanceName, setResponse, HttpResponse, ...props}) => ({
-  token: process.env.TOKEN || meta.token || token,
-  instanceName: process.env.INSTANCE_NAME || meta.instance || instanceName,
-  host: process.env.SYNCANO_HOST || meta.api_host || 'api.syncano.io',
-  spaceHost: process.env.SPACE_HOST || meta.space_host || 'syncano.space',
+global.META = global.META || {}
+
+export default ({
+  meta = {},
+  token,
+  instanceName,
+  setResponse,
+  HttpResponse,
+  ...props
+}) => ({
+  token: process.env.TOKEN || global.META.token || meta.token || token,
+  instanceName: process.env.INSTANCE_NAME ||
+    global.META.instance ||
+    meta.instance ||
+    instanceName,
+  host: process.env.SYNCANO_HOST ||
+    global.META.api_host ||
+    meta.api_host ||
+    'api.syncano.io',
+  spaceHost: process.env.SPACE_HOST ||
+    global.META.space_host ||
+    meta.space_host ||
+    'syncano.space',
   apiVersion: 'v2',
   meta,
   setResponse,
