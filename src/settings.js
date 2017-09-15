22c397c1613546d5f962c19a28fc363b724c0456
Kasper Mikiewicz <ideredpl@gmail.com>
2017-09-15 15:55:08 +0200

    fix: invalid env var

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 export default ({meta = {}, token, instanceName, setResponse, HttpResponse}) => ({
   token: process.env.TOKEN || meta.TOKEN || token,
   instanceName: process.env.INSTANCE_NAME || meta.INSTANCE_NAME || instanceName,
-  host: process.env.HOST || meta.HOST || 'api.syncano.io',
+  host: process.env.SYNCANO_HOST || meta.HOST || 'api.syncano.io',
   spaceHost: process.env.SPACE_HOST || meta.SPACE_HOST || 'syncano.space',
   apiVersion: 'v2',
   meta,
