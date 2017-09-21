e0e6444a1b7cf5a35ae3dad00b16620e205acac4
mkucharz <m@kucharz.net>
2017-09-21 20:43:21 +0200

    fix(init): when there is no meta

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ const server = (ctx = {}) => {
   const account = new Account(config)
   const instance = new Instance(config)
 
-  Logger.config = ctx.meta.debug
+  Logger.config = ctx.meta ? ctx.meta.debug : ''
 
   return {
     _class,
